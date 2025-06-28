0ffd937ee913f8345703e5234fe9ff72c9ad7a63
Atipat Pankong <atipat.p@lmwn.com>
2025-06-28 12:31:18 +0700

    fix warning

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/fonts", "nuxt-icon"],
+  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon"],
   vite: {
     plugins: [tailwindcss()],
   },
